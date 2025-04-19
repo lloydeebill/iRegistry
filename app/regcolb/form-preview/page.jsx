5657048c1ef20984c1ab76f9ad4afbe052633a1f
lloydeebill <lloydbill.lariosa@gmail.com>
2025-04-19 01:15:18 -0400

    fixed usesearchparams deployement error

--- app/regcolb/form-preview/page.jsx
+++ app/regcolb/form-preview/page.jsx
@@ -1,4 +1,5 @@
 "use client";
+export const dynamic = "force-dynamic";
 import { useSearchParams, useRouter } from "next/navigation";
 import { generateCertificatePDF } from "@/utils/generateCertificatePDF";
 import { supabase } from "@/lib/supabaseClient";
